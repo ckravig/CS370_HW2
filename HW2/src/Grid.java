5d592fdb3a996a98685cca947e4febb2ea5a264d
Collin Kravig <ckravig@gmail.com>
2022-11-10 20:10:47 -0600

    Create 2D Array Grid

--- HW2/src/Grid.java
+++ HW2/src/Grid.java
@@ -1,5 +1,31 @@
 public class Grid {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        // create 5x5 grid (2D array) from document provided
+        Integer[][] grid = {
+            {null,30,30,30,null},
+            {15,null,null,null,72},
+            {15,null,null,null,72},
+            {15,null,null,null,72},
+            {null,75,75,75,null}
+        };
+
+        // iterate through the 2D array, row by row by accessing the
+        // individual arrays within the 2D array
+        for (Integer[] rows: grid) {
+            // iterate through the given row to access the data
+            for(Integer data: rows) {
+                System.out.print("|");
+                if (data == null) {
+                    System.out.print("--");
+                }else{
+                    System.out.print(data);
+                }
+                
+            }
+            System.out.print("|");
+            System.out.println();
+        }
+
     }
 }
