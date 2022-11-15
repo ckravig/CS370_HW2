6969598f878b426395b3e55e5f003b54a5742fe5
Collin Kravig <ckravig@gmail.com>
2022-11-14 18:35:28 -0600

    multithreading running

--- HW2/src/Grid.java
+++ HW2/src/Grid.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Author: Collin Kravig
  * Class: CSCI 370
@@ -33,7 +36,7 @@ public class Grid{
     public static int tGrid = 0;
 
     // minimum number of "plates" in grid
-    public static int tPlates = 49;
+    public static int tPlates = 25;
 
     // top row value
     public static Float gTop = 30.00f;
@@ -52,7 +55,7 @@ public class Grid{
     //--------------------------//
 
     // thread count
-    public static int numChild = 1;
+    public static int numChild = 4;
     public static String threading = "";
 
     //-------------------------------------------------------------------------------------------//
@@ -75,34 +78,42 @@ public class Grid{
         int cCols = 0;
 
         // figure out how large the 2D array needs to be
-        // make sure grid is divisable by 2
-        while ((cPlates < tPlates) || (cRows%2 != 0)) {
+        // make sure grid is divisable by 4 to be optimized for 
+        // 4 threads
+        while ((cPlates < tPlates) || (cRows%4 != 0)) {
             cPlates = 0;
             cRows++;
             cCols++;
             cPlates = cRows * cCols;
         }
 
+        Float[][] grid = new Float[cRows][cCols];
+
         // calculate thread range
-        int range = cRows/numChild;
+        int range = cRows/4;
 
         List<Thread> threads = new ArrayList<Thread>();
 
-        // store grid starting point
+        // store grid starting row for threads
         int begin = 0;
 
         // Spawn children processes
         for (int i=0; i<numChild; i++) {
-            Thread t = new Thread(new Child(begin, begin+range));
+            Thread t = new Thread(new Child(begin, begin+range, grid));
             t.start();
             threads.add(t);
-            begin += range + 1;
+            begin += range;
         }
 
-        Float[][] grid = new Float[cRows][cCols];
+        // Wait for children to finish
+        for (Thread t : threads) {
+            try { t.join(); } catch (InterruptedException e) { e.printStackTrace(); }
+        }
+        System.out.println("All Children Done: " + numChild);
+    
 
         // fill grid
-        fillGrid(grid);
+        // fillGrid(grid);
 
         // create 5x5 grid (2D array) from document provided
         // Float[][] oGrid = {
@@ -127,16 +138,16 @@ public class Grid{
 
         
 
-        // first grid average
-        calcAvg(grid);
+        // // first grid average
+        // calcAvg(grid);
 
-        // repeat average to obtain initial error
-        reCalcAvg(grid);
+        // // repeat average to obtain initial error
+        // reCalcAvg(grid);
 
-        // repeat average calculation until error is less than 5
-        while (tError > 5) {
-            reCalcAvg(grid);
-        }
+        // // repeat average calculation until error is less than 5
+        // while (tError > 5) {
+        //     reCalcAvg(grid);
+        // }
         
 
         //--------------------// 
@@ -147,7 +158,7 @@ public class Grid{
         // System.out.println("Final Grid: ");
         // System.out.println();
 
-        // printGrid(grid);
+        printGrid(grid);
 
         // determine threading type
         if (numChild != 1) {
@@ -156,7 +167,9 @@ public class Grid{
             threading = "Single Thread";
         }
 
-        tAverage(grid);
+        //tAverage(grid);
+
+        System.out.println("Total Calulations = " + calcs);
 
         System.out.println();
         System.out.print("Type: " + threading + "; Size=" + tGrid);
@@ -169,17 +182,6 @@ public class Grid{
 
     }
 
-    class Child implements Runnable {
-
-
-        @Override
-        public void run() {
-            // TODO Auto-generated method stub
-            
-        }
-
-        
-    }
 
     //-------------------------------------------------------------------------------------------//
 
@@ -195,6 +197,9 @@ public class Grid{
      * @param grid the 2D array that holds the data
      */
     public static void calcAvg(Float[][] grid) {
+
+        int begin;
+        int end;
         
         //----------------------// 
         //  Calculate Averages  //
@@ -265,6 +270,67 @@ public class Grid{
         calcs++;
     }
 
+    public static void calcAvgThreadTest(Float[][] grid, int begin, int end) {
+
+        //----------------------// 
+        //  Calculate Averages  //
+        //----------------------//
+
+        // iterate through the 2D array, row by row by accessing the
+        // individual arrays within the 2D array
+        for (int r = begin; r < end; r++) {
+
+            // iterate through the given row to access the data
+            for(int c = 0; c < grid.length; c++) {
+
+                // check for empty spots in the grid
+                if (grid[r][c] == null && (c != 0 && c != (grid.length - 1)) && (r != 0 && r != (grid.length - 1))) {
+                    
+                    // divisor for average calculation
+                    Float divisor = 4.00f;
+
+                    // average calculation temp values
+                    Float top = 0.00f;
+                    Float left = 0.00f;
+                    Float right = 0.00f;
+                    Float bot = 0.00f;
+
+                    // check top number
+                    if (r != 0 && grid[(r-1)][c] != null) {
+                        top = grid[(r-1)][c];
+                    }
+
+                    // check bottom number
+                    if (r != (end - 1) && grid[(r+1)][c] != null) {
+                        bot = grid[(r+1)][c];
+                    }
+
+                    // check left number
+                    if (c != 0 && grid[r][(c-1)] != null) {
+                        left = grid[r][(c-1)];
+                    }
+
+                    // check right number
+                    if (c != (grid.length - 1) && grid[r][(c+1)] != null) {
+                        right = grid[r][(c+1)];
+                    }
+
+                    // check for corners to adjust divisor
+                    if (c == 0 || r == 0) {
+                        divisor = 2.00f;
+                    }
+
+                    Float avg = ((top + bot + left + right) / divisor);
+
+                    // store average in array and print to new grid
+                    grid[r][c] = avg;
+                }
+            }
+        }
+
+        calcs++;
+    }
+
     //-------------------------------------------------------------------------------------------// 
 
     //----------------------------------// 
@@ -278,6 +344,9 @@ public class Grid{
      * @param grid the 2D array that holds the data
      */
     public static void reCalcAvg(Float[][] grid) {
+
+        int begin;
+        int end;
         
         tError = 0.00f;
 
@@ -355,6 +424,69 @@ public class Grid{
         
     }
 
+    public static void reCalcAvgThreadTest(Float[][] grid, int begin, int end) {
+        
+        tError = 0.00f;
+
+        //----------------------// 
+        //  Calculate Averages  //
+        //----------------------//
+
+        // iterate through the 2D array, row by row by accessing the
+        // individual arrays within the 2D array
+        for (int r = begin; r < end; r++) {
+
+            // iterate through the given row to access the data
+            for(int c = 0; c < grid.length; c++) {
+
+
+                // check that we aren't on the sides of the grid
+                if ((c != 0 && c != (grid.length - 1)) && (r != 0 && r != (grid.length - 1))) {
+                    
+                    // divisor for average calculation
+                    Float divisor = 4.00f;
+
+                    // average calculation temp values
+                    Float top = 0.00f;
+                    Float left = 0.00f;
+                    Float right = 0.00f;
+                    Float bot = 0.00f;
+
+                    // check top number
+                    if (r != 0) {
+                        top = grid[(r-1)][c];
+                    }
+
+                    // check bottom number
+                    if (r != (end - 1)) {
+                        bot = grid[(r+1)][c];
+                    }
+
+                    // check left number
+                    if (c != 0) {
+                        left = grid[r][(c-1)];
+                    }
+
+                    // check right number
+                    if (c != (grid.length - 1)) {
+                        right = grid[r][(c+1)];
+                    }
+
+                    Float avg = ((top + bot + left + right) / divisor);
+
+                    // calculate error (|new avg - old avg|)
+                    Float error = Math.abs(avg - grid[r][c]);
+
+                    // append error to total error
+                    tError += error;
+
+                    // store average in array and print to new grid
+                    grid[r][c] = avg;
+                }
+            }
+        }
+    }
+
     //-------------------------------------------------------------------------------------------//
 
     public static void tAverage(Float[][] grid){
@@ -386,12 +518,17 @@ public class Grid{
             }
             r++;
         }
+
+        // possibly change for multithreading
         tAvg = total / (insideGrid);
     }
 
     //-------------------------------------------------------------------------------------------//
 
     public static void fillGrid(Float[][] grid) {
+
+        int begin;
+        int end;
 
         // current iterative column and row
         int c = 0; // column
@@ -433,6 +570,43 @@ public class Grid{
         }
     }
 
+    public static void fillGridThreadTest(Float[][] grid, int begin, int end) {
+
+        // iterate through the 2D array, row by row by accessing the
+        // individual arrays within the 2D array
+        for (int r = begin; r < end; r++) {
+
+            
+            // iterate through the given row to access the data
+            for(int c = 0; c < grid.length; c++) {
+                        
+                // populate top row
+                if(r == 0 && (c != 0 && c != (grid.length - 1))){
+                    grid[r][c] = gTop;
+                }
+
+                // populate bottom row
+                if(r == (grid.length - 1) && (c != 0 && c != (grid.length - 1))){
+                    grid[r][c] = gBot;
+                }
+
+                // populate left row
+                if(c == 0 && (r != 0 && r != (grid.length - 1))){
+                    grid[r][c] = gLeft;
+                }
+
+                // populate right row
+                if(c == (grid.length - 1) && (r != 0 && r != (grid.length - 1))){
+                    grid[r][c] = gRight;
+                }
+
+                
+            }
+            
+        }
+
+    }
+
     //-------------------------------------------------------------------------------------------//
 
     public static void printGrid(Float[][] grid) {
@@ -452,6 +626,46 @@ public class Grid{
             System.out.print("|");
             System.out.println();
         }
+    }
+
+    
+}
+
+
+class Child implements Runnable {
+
+    private int begin;
+    private int end;
+
+    private Float[][] tGrid;
+
+    public Child(int begin, int end, Float[][] grid) {
+        this.begin = begin;
+        this.end = end;
+        this.tGrid = grid;
+    }
+    
+
+    @Override
+    public void run() {
+                
+        Grid.fillGridThreadTest(tGrid, begin, end);
+
+        
+
+        Grid.calcAvgThreadTest(tGrid, begin, end);
+
+        
+        while (Grid.tError > 5) {
+            Grid.reCalcAvgThreadTest(tGrid, begin, end);
+
+            // add to total repeated calculations total
+            Grid.calcs++;
+        }
+        
+        // Grid.reCalcAvgThreadTest(tGrid, begin, end);
+        
+        
     }
 
     
